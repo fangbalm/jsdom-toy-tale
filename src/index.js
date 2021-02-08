3142f63f224bfb1f39366d40de377f4c63bd2a03
Lucius VanSlyke <lu.vanslyke@gmail.com>
2021-02-08 14:55:16 -0500

    third commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const toyFormContainer = document.querySelector(".container");
 const toyForm = toyFormContainer.querySelector(".add-toy-form")
 const url = `http://localhost:3000/toys`
 const toyCollection = document.querySelector('#toy-collection')
+// const likeBtn = document.getElementsByClassName('like-btn')
+//   console.log(likeBtn)
 
 
 /* APP */
@@ -38,7 +40,7 @@ function getAllCards(){
       card.innerHTML = `
       <h2>${toy.name}</h2>
       <img src=${toy.image} class="toy-avatar" />
-      <p> ${toy.likes} Likes </p>
+      <p class="react-count"> ${toy.likes} Likes </p>
       <button class="like-btn">Like <3</button>
     </div>`
      
@@ -80,8 +82,35 @@ function getAllCards(){
   .then(newToy => console.log(newToy))
 
  })
- 
-  console.log(likeBtn)
+
+ toyCollection.addEventListener('click', function(event){
+   const card = event.target.closest('div.card')
+   if (event.target.className === 'like-btn')
+   console.log('Like button clicked!!')
+
+   const likesDisplay = card.querySelector('.react-count')
+   const likes = parseInt(likesDisplay.textContent)
+
+    fetch(`${url}/${card.dataset.id}`, {
+      method: 'PATCH', 
+      headers: {
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify({likes: likes + 1})
+
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+
+    
+
+  //  const likesDisplay = card
+  //  console.log(likesDisplay)
+  //  const likes = parseInt(likesDisplay)
+  //  console.log(likes)
+ })
 });
 
 
